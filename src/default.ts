56a75cd71b3336b0cfd5aeb1f268d9ccdaf04a03
速龙 <54501341+fastdragon18@users.noreply.github.com>
2023-03-10 14:40:22 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,5 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站仅用于演示，填入自己的 key 才可使用。
-- 由 [是枭](https://blog.evv1.com) 搭建功能强大，响应速度快，欢迎自部署。
+- 本站功能强大，响应速度快，欢迎自部署。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
